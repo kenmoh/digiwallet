b45f5c76efb60b77436d34f3441ae49d14c7e03b
kenmoh <kenneth.aremoh@gmail.com>
2023-09-14 21:09:04 +0100

    update delete route

--- app/routers/topup_router.py
+++ app/routers/topup_router.py
@@ -17,17 +17,27 @@ top_up_router = APIRouter(prefix='/api/top-up', tags=['Top Up'])
 
 @top_up_router.get('', status_code=status.HTTP_200_OK)
 def get_user_top_ups(user_id: str, db: Session = Depends(get_db)) -> list[TopUpResponseSchema]:
+    """
+    - Get all top-up by user
+    """
     return topup_wallet.user_top_ups(user_id, db)
 
 
 @top_up_router.post('/{wallet_address}', status_code=status.HTTP_201_CREATED)
 def top_up_wallet(wallet_address: uuid.UUID, top_up: TopUpSchema, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)) -> WalletResponseSchema:
+    """
+    - User wallet top-up
+    """
     if current_user.wallet.wallet_address == wallet_address:
         return topup_wallet.top_up_db_wallet(wallet_address=wallet_address, top_up=top_up, db=db, user=current_user)
 
 
 @top_up_router.get('/{top_up_id}', status_code=status.HTTP_200_OK)
 def get_top_up_details(top_up_id: str, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)) -> TopUpResponseSchema:
+    """
+    - Get the top-up details of wallet
+    """
+
     if not current_user:
         error_response.invalid_exception('Unauthorized')
     return topup_wallet.top_up_details(top_up_id, db)
--- app/services/transfer.py
+++ app/services/transfer.py
@@ -11,8 +11,12 @@ from app.utils import error_response
 
 
 def make_transfer(wallet_address: uuid.UUID, transfer: TransferSchema, user: User, db: session):
+    """
+    - Helper function to transfer from wallet tp wallet
+    """
     transfer_to_wallet = db.query(Wallet).filter(Wallet.wallet_address == wallet_address).first()
     transfer_from_wallet = db.query(Wallet).filter(Wallet.wallet_address == user.wallet.wallet_address).first()
+
     if not transfer_to_wallet:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'{transfer_to_wallet.id} not Found')
 
@@ -22,7 +26,7 @@ def make_transfer(wallet_address: uuid.UUID, transfer: TransferSchema, user: Use
     if transfer_from_wallet.wallet_address == transfer_to_wallet.wallet_address:
         error_response.invalid_exception('Not allowed. You cannot transfer to your own wallet!')
 
-    if transfer.amount <= 500:
+    if transfer.amount <= 499:
         error_response.invalid_exception('Invalid amount. Please enter a minimum amount of N 500!')
 
     try:
@@ -53,12 +57,21 @@ def make_transfer(wallet_address: uuid.UUID, transfer: TransferSchema, user: Use
 
 
 def user_transfers(user_id: str, db: Session):
+    """
+    - Helper function to get transfers by user
+    """
     return db.query(Transfer).filter(Transfer.user_id == user_id).all()
 
 
 def transfer_details(transfer_id: str, db: Session):
+    """
+    - Get transfer details
+    """
     return db.query(Transfer).filter(Transfer.id == transfer_id).first()
 
 
 def all_transfers(db: Session):
+    """
+    Get all transfers from database
+    """
     return db.query(Transfer).all()
